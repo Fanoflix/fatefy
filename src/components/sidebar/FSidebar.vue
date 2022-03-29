e07a80928deecdbaadff1a43f9b31058cb15dfee
Ammar Bin Nasir <majidammar428@gmail.com>
2022-03-29 15:48:46 +0500

    Sidebar made independent.

--- src/components/sidebar/FSidebar.vue
+++ src/components/sidebar/FSidebar.vue
@@ -1,18 +1,21 @@
 <template>
-  <FContainer
-    width="220"
-    height="100vh"
-    class="side-bar"
-    heading="test"
-  ></FContainer>
+  <section class="side-bar" :class="[{ dark: isDark }]"></section>
 </template>
 
 <script setup>
-/* 
-  TODO Make Sidebar retractable 
-  TODO Make Sidebar a separate component insteadof just a container
+/*
+  TODO Make Sidebar retractable
 */
-import FContainer from "../container/FContainer.vue";
+
+// Imports
+import { storeToRefs } from "pinia";
+import { useThemeStore } from "../../stores/theme";
+
+// State
+
+// Reactive State
+
+const { isDark } = storeToRefs(useThemeStore());
 </script>
 
 <style scoped lang="scss">
@@ -23,7 +26,14 @@ import FContainer from "../container/FContainer.vue";
   top: $nav-height;
   left: 0;
 
+  background-color: $color-background;
+  border-right: 1px solid $white-soft;
   width: 220px;
   height: 100vh;
+}
+
+.dark.side-bar {
+  background-color: $color-background-dark;
+  border-right: 1px solid $black-soft;
 }
 </style>
